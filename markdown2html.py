6a02c7294f04d86e68873b5b54c01cd3a44dd0dc
syou7 <y.saber@outlook.fr>
2024-04-17 14:56:38 +0200

    add exit(0)

--- markdown2html.py
+++ markdown2html.py
@@ -3,21 +3,21 @@
 import sys
 import os
 
-def check_arguments():
+def check_arg():
     """ Check if the number of arguments is correct"""
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
 
-# Check if the input file exists
     if not os.path.isfile(sys.argv[1]):
         print(f"Missing {sys.argv[1]}", file=sys.stderr)
         sys.exit(1)
+    exit(0)
 
 
 
 
 
 if __name__ == '__main__':
-    check_arguments()
+    check_arg()
 
